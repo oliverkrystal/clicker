c4b2d0aba8b168239bea6233ed9e1f52a0f64e27
OliverKrystal <oliverk@soliloquyforthefallen.net>
2013-01-28 09:36:34 -0600

    Basically, write a program with a kindy of crappy debounce system

--- program/program.ino
+++ program/program.ino
@@ -1,19 +1,23 @@
-void setup() {
-  int currentThreshHold = 0;
-  int currentPhotocellStatus = 0;
+int photocell = 23;
+int led = 22;
+int potentiometer = 21;
 
-  int photocell = 23;
-  int led = 22;
-  int potentiometer = 21;
+
+
+void setup() {
+pinMode(led, OUTPUT);
 }
 
 void loop() {
 	//Start checking to see if we are moving or not
 
-	if ((analogRead(photcell)) > (analogRead(potentiometer))){
-		digitalWrite(led, HIGH);
-		Keyboard.print("n");
-		digitalWrite(led, low);
+	if ((analogRead(photocell)) > (analogRead(potentiometer))){
+		delay(1);
+		if ((analogRead(photocell)) > (analogRead(potentiometer))){
+			digitalWrite(led, HIGH);
+			Keyboard.print("n");
+			digitalWrite(led, LOW);
+		}
 	}
 	delay(350);
 
