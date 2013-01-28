a3c3df97f735b7df5870d634941fd6919889504c
OliverKrystal <oliverk@soliloquyforthefallen.net>
2013-01-28 08:20:39 -0600

    setup pins and housekeeping

--- program/program.ino
+++ program/program.ino
@@ -1,16 +1,17 @@
-#include "usb_keyboard.h"
-
-//setup display
-serLCD lcd(12);
-
-
 void setup() {
-  usb_init()
+  int currentThreshHold = 0;
+  int currentPhotocellStatus = 0;
+
+  int photocell = 23;
+  int led = 22;
+  int potentiometer = 21;
 }
 
 void loop() {
 	//Start checking to see if we are moving or not
-	analogRead(23)
+
+	analogRead(photcell);
+
 	delay(350);
 
 }
