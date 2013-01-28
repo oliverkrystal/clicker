68638c83db8e45558e9b97fad53ec8dc7ddba9af
OliverKrystal <oliverk@soliloquyforthefallen.net>
2013-01-28 08:17:45 -0600

    initial commit

---
+++ program/program.ino
@@ -0,0 +1,18 @@
+#include "usb_keyboard.h"
+
+//setup display
+serLCD lcd(12);
+
+
+void setup() {
+  usb_init()
+}
+
+void loop() {
+	//Start checking to see if we are moving or not
+	analogRead(23)
+	delay(350);
+
+}
+
+
